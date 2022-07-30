221a68265e5f85dda0f2f0b111f6ca438a13852d
Leonomics <leocv97@gmail.com>
2022-07-30 16:08:16 +0200

    add event listener for play_button. add console log when user click a square

--- myscript.js
+++ myscript.js
@@ -1,18 +1,46 @@
 let dimensioneGriglia = 10;
 let numeroCelle = dimensioneGriglia ** 2;
-
-
 const tabelloneEl = document.querySelector('.tabellone');
-for(let i = 0; i<numeroCelle; i++){
-    const cella = getSquareElement();
-    tabelloneEl.append(cella);
+const playButtonEl = document.getElementById('play_button');
+
+playButtonEl.addEventListener('click', showGrid);
+
+
+
+function showGrid(){
+    tabelloneEl.style.visibility = "visible";
+    generaGriglia(dimensioneGriglia, tabelloneEl);
 }
+
+function generaGriglia(dimensione, tabellone){
+    const numeroCelle = dimensione ** 2;
+    //const div = document.createElement('div');
+
+    for(let i = 0; i<numeroCelle; i++){
+        const cella = getSquareElement();
+        cella.innerHTML = i + 1;
+        //div.append(cella);
+        tabellone.append(cella);    
+    }
+
+    //return div.innerHTML;
+
+}
+
 
 function getSquareElement(){
     const square = document.createElement('div');
     square.classList.add('square');
+    square.addEventListener('click', clickHandler)
 
     return square;
 }
 
+function clickHandler(){
 
+    const square = this;
+    square.classList.toggle('clicked');
+    square.removeEventListener('click', clickHandler);
+    console.log("cella numero: " + parseInt(square.innerHTML))
+
+}
